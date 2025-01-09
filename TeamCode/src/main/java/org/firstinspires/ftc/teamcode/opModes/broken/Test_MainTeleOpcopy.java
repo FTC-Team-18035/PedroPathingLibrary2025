b5e40096126458cf93a5132e20c091b9e86744c0
FTC18035 <ftc18035@gmail.com>
2025-01-09 14:34:54 -0500

    Fixed some packages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/broken/Test_MainTeleOpcopy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/broken/Test_MainTeleOpcopy.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.opModes.broken;
 
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
