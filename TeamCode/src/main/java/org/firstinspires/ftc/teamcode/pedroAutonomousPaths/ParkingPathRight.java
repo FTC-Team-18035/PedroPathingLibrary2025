1b64cf750714f98445817e407a405cbd0bd70608
FTC18035 <ftc18035@gmail.com>
2025-01-10 19:39:30 -0500

    Updated to initialize the Intake and outtake so that it stays in the proper position for TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroAutonomousPaths/ParkingPathRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroAutonomousPaths/ParkingPathRight.java
@@ -9,6 +9,7 @@ import com.pedropathing.util.Constants;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
@@ -20,6 +21,24 @@ public class ParkingPathRight extends OpMode {
     private Follower follower;
     private Timer pathTimer, actionTimer, opModeTimer;
     private int pathState;
+
+    public double LeftServo;
+    public double RightServo;
+    private double V4Bpos = 1;
+    private double Flex = 0;
+    private double Yaw = 0;
+
+    Servo RightIntakeWrist; // Chub Port 1 // Increments Using Dpad Side Buttons?
+    Servo LeftIntakeWrist;   // Chub Port 2 // Ideally Stick Controlled
+    Servo IntakeV4B;     // Chub Port 3 // Preset To Swing Out With X
+    // Servo LeftIntakeV4B = hardwareMap.servo.get("Left Intake V4B");       // Chub Port 4 // --------------------------
+    //  Servo RightHook = hardwareMap.servo.get("Right Hook");                // Chub Port 5 // Linked To LeftHook Activated At The Same Time
+
+    Servo IntakeClaw;
+
+    Servo OuttakeClaw;            // Ehub Port 0 // If Slides Up O Activates This Claw
+    Servo OuttakeWrist;          // Ehub Port 1 // Preset To Go To Delivery Position With Triangle
+    Servo OuttakeV4B;
 
     private final Pose startPose = new Pose(10, 57, Math.toRadians(0));
 
@@ -51,7 +70,11 @@ public class ParkingPathRight extends OpMode {
         autonomousPathUpdate();
 
         if(opModeTimer.getElapsedTimeSeconds() >= 5) {
-            requestOpModeStop();
+            OuttakeV4B.setPosition(.5);
+            IntakeV4B.setPosition(.8);
+            if(opModeTimer.getElapsedTimeSeconds() >= 7) {
+                requestOpModeStop();
+            }
         }
 
         // Feedback to Driver Hub
@@ -65,12 +88,44 @@ public class ParkingPathRight extends OpMode {
     @Override
     public void init() {
         pathTimer = new Timer();
-        opModeTimer = new Timer();
-        opModeTimer.resetTimer();
+
+        RightIntakeWrist = hardwareMap.servo.get("Right Intake Wrist"); // Chub Port 1 // Increments Using Dpad Side Buttons?
+        LeftIntakeWrist = hardwareMap.servo.get("Left Intake Wrist");   // Chub Port 2 // Ideally Stick Controlled
+        IntakeV4B = hardwareMap.servo.get("Intake V4B");     // Chub Port 3 // Preset To Swing Out With X
+        // Servo LeftIntakeV4B = hardwareMap.servo.get("Left Intake V4B");       // Chub Port 4 // --------------------------
+        //  Servo RightHook = hardwareMap.servo.get("Right Hook");                // Chub Port 5 // Linked To LeftHook Activated At The Same Time
+
+        IntakeClaw = hardwareMap.servo.get("Intake Claw");
+
+        OuttakeClaw = hardwareMap.servo.get("Outtake Claw");            // Ehub Port 0 // If Slides Up O Activates This Claw
+        OuttakeWrist = hardwareMap.servo.get("Outtake Wrist");          // Ehub Port 1 // Preset To Go To Delivery Position With Triangle
+        OuttakeV4B = hardwareMap.servo.get("Outtake V4B");
+
+        LeftServo = Math.max(0, Math.min(1, Flex - (.5 * Yaw)));
+        RightServo = Math.max(0, Math.min(1, Flex + (.5 * Yaw)));
+
+        LeftIntakeWrist.setPosition(LeftServo);    // Sets the intake wrist to the starting position // Left is 0 Right is 1
+        RightIntakeWrist.setPosition(RightServo);   // Sets the intake wrist to the starting position // Left is 0 Right is 1
+        IntakeV4B.setPosition(.8);   // Sets the intake virtual four bar to the starting position
+        //RightIntakeV4B.setPosition(1);  // Sets the intake virtual four bar to the starting position
+
+        IntakeClaw.setPosition(0);    // Closes Intake Claw
+
+        OuttakeClaw.setPosition(0);   // Closes Outtake Claw
+
+        OuttakeWrist.setPosition(0);    // Sets the outtake wrist to the starting position
+
+        OuttakeV4B.setPosition(1);
 
         Constants.setConstants(FConstants.class, LConstants.class);
         follower = new Follower(hardwareMap);
         follower.setStartingPose(startPose);
         buildPaths();
+    }
+
+    @Override
+    public void start() {
+        opModeTimer = new Timer();
+        opModeTimer.resetTimer();
     }
 }
