291422d263930a40895fc08ef4af61018b92c2bd
FTC A <ftc18035@gmail.com>
2025-02-07 18:11:56 -0500

    Updated FConstants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -20,6 +20,47 @@ public class FConstants {
         FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
         FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
 
+        FollowerConstants.mass = 16.60148;
+
+        FollowerConstants.xMovement = 54.8677;
+        FollowerConstants.yMovement = 38.0781;
+
+        FollowerConstants.forwardZeroPowerAcceleration = -32.3427;
+        FollowerConstants.lateralZeroPowerAcceleration = -67.989;
+
+        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.31, 0, 0, 0.001);
+        FollowerConstants.useSecondaryTranslationalPID = false;
+        FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
+
+        FollowerConstants.headingPIDFCoefficients.setCoefficients(3,0,0,0.125);
+        FollowerConstants.useSecondaryHeadingPID = false;
+        FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
+
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(.005,0,.00027,0,0);
+        FollowerConstants.useSecondaryDrivePID = false;
+        FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
+
+        FollowerConstants.zeroPowerAccelerationMultiplier = 4;
+        FollowerConstants.centripetalScaling = 0.001;
+
+        FollowerConstants.pathEndTimeoutConstraint = 500;
+        FollowerConstants.pathEndTValueConstraint = 0.995;
+        FollowerConstants.pathEndVelocityConstraint = 0.1;
+        FollowerConstants.pathEndTranslationalConstraint = 0.1;
+        FollowerConstants.pathEndHeadingConstraint = 0.007;
+        /*
+        FollowerConstants.localizers = Localizers.TWO_WHEEL;
+
+        FollowerConstants.leftFrontMotorName = "Front Left";
+        FollowerConstants.leftRearMotorName = "Back Left";
+        FollowerConstants.rightFrontMotorName = "Front Right";
+        FollowerConstants.rightRearMotorName = "Back Right";
+
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+
         FollowerConstants.mass = 16.72148;
 
         FollowerConstants.xMovement = 47.911; //54.8677;
@@ -48,5 +89,6 @@ public class FConstants {
         FollowerConstants.pathEndVelocityConstraint = 0.1;
         FollowerConstants.pathEndTranslationalConstraint = 0.1;
         FollowerConstants.pathEndHeadingConstraint = 0.007;
+        */
     }
 }
