f4073985495a4b79deece17ddb427214031b33d6
FTC A <ftc18035@gmail.com>
2025-02-07 18:18:21 -0500

    Updated LConstants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -7,6 +7,18 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 public class LConstants {
     static {
         TwoWheelConstants.forwardTicksToInches = .002;
+        TwoWheelConstants.strafeTicksToInches = .002;
+        TwoWheelConstants.forwardY = -3.623;
+        TwoWheelConstants.strafeX = 3.770;
+        TwoWheelConstants.forwardEncoder_HardwareMapName = "Front Right";
+        TwoWheelConstants.strafeEncoder_HardwareMapName = "Back Right";
+        TwoWheelConstants.forwardEncoderDirection = Encoder.REVERSE;
+        TwoWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        TwoWheelConstants.IMU_HardwareMapName = "imu"; // TwoWheelConstants.IMU_HardwareMapName = "navX";
+        TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.
+                LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
+        /*
+        TwoWheelConstants.forwardTicksToInches = .002;
         TwoWheelConstants.strafeTicksToInches = .0015; //.002;
         TwoWheelConstants.forwardY = -3.623;
         TwoWheelConstants.strafeX = 3.770;
@@ -17,6 +29,8 @@ public class LConstants {
         TwoWheelConstants.IMU_HardwareMapName = "imu";
         TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.
                 LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
+
+         */
     }
 }
 
